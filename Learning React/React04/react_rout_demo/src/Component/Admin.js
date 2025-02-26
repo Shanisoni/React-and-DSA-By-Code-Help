298dde0a2188aae24659c7a90c822bc9529ede69
Shanisoni <shanisoni7084@gmail.com>
2025-02-26 19:13:41 +0530

    commited

--- Learning React/React04/react_rout_demo/src/Component/Admin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Admin() {
-  return (
-    <div> I am a Good Admin</div>
-  )
-}
-
-export default Admin
--- Learning React/React05/React06/Chintu/react07/redux-toolkit/src/ReduxToolkit/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'; // Ensure correct import path for @reduxjs/toolkit
-import counterReducer from './CounterSlice'; // Import the default exported reducer from CounterSlice
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer, // Use the reducer from counterSlice
-  },
-});
